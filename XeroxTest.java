7f8bd28ed387dddd186e5ec4af167a2b2748b063
kumargit <kumar.c.swain@gmail.com>
2018-03-17 18:47:58 +0530

    Add files via upload

---
+++ XeroxTest.java
@@ -0,0 +1,15 @@
+package com.xerox.company;
+
+public class XeroxTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10 + 30 + "test" + (40 + 50));
+		System.out.println(10 + 30 + "test" + 40 + 50);
+		for (int i = 0; true; i++) {
+			System.out.println("Hello");
+			break;
+		}
+	}
+
+}
